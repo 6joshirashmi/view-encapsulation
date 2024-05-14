62699c3749e0658acaced0a7389df4f46b9b58ab
rashmi <6joshirashmi@gmail.com>
2024-05-14 19:00:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShadowdomcompoComponent } from './shadowdomcompo/shadowdomcompo.component';
+import { NonecompoComponent } from './nonecompo/nonecompo.component';
+import { EmulatedcompoComponent } from './emulatedcompo/emulatedcompo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ShadowdomcompoComponent,NonecompoComponent,EmulatedcompoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
   title = 'viewencapsulation';
--- src/app/shadowdomcompo/shadowdomcompo.component.ts
+++ src/app/shadowdomcompo/shadowdomcompo.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shadowdomcompo',
   standalone: true,
   imports: [],
   templateUrl: './shadowdomcompo.component.html',
-  styleUrl: './shadowdomcompo.component.css'
+  styleUrl: './shadowdomcompo.component.css',
+ 
 })
 export class ShadowdomcompoComponent {
 
